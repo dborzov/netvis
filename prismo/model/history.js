d3d7103c28290dfc16ad7d6bb7974b4b82c702bd
Peter Borzov <tihoutrom@gmail.com>
2014-11-14 08:54:43 -0500

    Assign eventID as timestamp#count where count is # of events with same timestamp

--- prismo/model/history.js
+++ prismo/model/history.js
@@ -5,9 +5,22 @@ NetVisHistory = function() {
 	BaseNetVisModel.apply(this); // History class inherits from baseModel
 	
 	this.loadEvent = function(obj, momentTime) {
+		log.console('loading event ', obj);
 		obj._t = momentTime;
 		obj.time = momentTime.toISOString();
-		this.load(obj,momentTime.toISOString());
+		var isoTime = momentTime.toISOString();
+		var i=0;
+		var eventID = isoTime; 
+		// eventID to be unique and contain timestamp
+		// of form "<timestamp>#3" where 3 is count of events with
+		// the same timestamp
+		log.console(eventID);
+		while (this.asObject[eventID]){
+			i++;
+			eventID = isoTime + "#" + i;
+		}
+		obj.id = eventID;
+		this.load(obj);
 	};
 	// add default time margin moments
 	this.loadEvent({"tag":"start"},moment("1970-01-01"));	
