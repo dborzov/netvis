d408b9b1a60a35375b4ba87712c7d123a7bd9ccd
Peter Borzov <tihoutrom@gmail.com>
2014-11-19 19:21:34 -0500

    Clarify why check for no nodes loaded in nodes.updateAll()

--- prismo/model/node.js
+++ prismo/model/node.js
@@ -7,10 +7,10 @@ NetVisNodes = function() {
 	self.updateAll = function() {
 		// generate default node's positioning coordinates on canvas
 		if (self.asArray.length === 0) {
-			// when no nodes loaded, 
+			// when no nodes loaded, computing arc variable would involve dividing by zero
 			return;
 		}
-			arc = 2 * Math.PI / self.asArray.length;
+		var	arc = 2 * Math.PI / self.asArray.length;
 		for (var i=0; i< self.asArray.length; i++) {
 			self.asArray[i]._x = 0.5 + 0.3 * Math.sin(i*arc);
 			self.asArray[i]._y = 0.5 - 0.3 * Math.cos(i*arc);
