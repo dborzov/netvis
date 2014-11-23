67da2728534b0ed19614b30ebda804ce4a406bde
Peter Borzov <tihoutrom@gmail.com>
2014-11-23 00:00:19 -0500

    Unittest IPFS converting for messageSent-type entry

--- test/ipfs.js
+++ test/ipfs.js
@@ -9,28 +9,6 @@ var NetVisEntry = "{\"event\":\"sentMessage\",\"loggerID\"EiCsOiFNw0RDqkoNuVeGtG
 
 var adapterIPFS = netvis.NetVis.prototype.adapterIPFS;
 tap.test('\nIPFS adapter: parsing sentMessage', function(t) {
-
-
-		// convert 
-		// Version should be detectable despite extra characters
-		[
-		['1.2.3', '1.2.3'],
-		[' 1.2.3 ', '1.2.3'],
-		[' 1.2.3-4 ', '1.2.3-4'],
-		[' 1.2.3-pre ', '1.2.3-pre'],
-		[' =v1.2.3 ', '1.2.3'],
-		['v1.2.3', '1.2.3'],
-		[' v1.2.3 ', '1.2.3'],
-		['\t1.2.3', '1.2.3'],
-		['>1.2.3', null],
-		['~1.2.3', null],
-		['<=1.2.3', null],
-		['1.2.x', null]
-		].forEach(function(tuple) {
-		var range = tuple[0];
-		var version = tuple[1];
-		var msg = 'clean(' + range + ') = ' + version;
-		t.equal(clean(range), version, msg);
-		});
+		t.equal(adapterIPFS.convert(IPFSEntry, console), NetVisEntry);
 		t.end();
 });
