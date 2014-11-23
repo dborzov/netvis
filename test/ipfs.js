a982d037b88954eac5f811ee88f8b96e643abf89
Peter Borzov <tihoutrom@gmail.com>
2014-11-23 01:32:39 -0500

    Rename netvis -> NetVis

--- test/ipfs.js
+++ test/ipfs.js
@@ -1,7 +1,7 @@
 var tap = require('tap');
 
 
-var netvis = require('../dist/netvis.js');
+var netvis = require('../dist/NetVis.js');
 
 var IPFSEntry = "{\"event\":\"sentMessage\",\"localPeer\":{\"id\":\"EiCsOiFNw0RDqkoNuVeGtGoz4JrHVdLoZhOBhU7dbaSOCg==\",\"latency\":0},\"message\":{\"type\":\"PING\"},\"system\":\"dht\",\"time\":\"2014-11-16T14:04:45.554426994Z\",\"remotePeer\":{\"id\":\"EiCwSlfUDsoTiAnxOadrEgRDM8N0A5HJvxzp2OIaeSEL/Q==\",\"latency\":0}}";
 var NetVisEntry = "{\"event\":\"sentMessage\",\"loggerID\"EiCsOiFNw0RDqkoNuVeGtGoz4JrHVdLoZhOBhU7dbaSOCg==\",\"message\":{\"type\":\"PING\"},\"system\":\"dht\",\"time\":\"2014-11-16T14:04:45.554426994Z\",\"destinationID\":\"EiCwSlfUDsoTiAnxOadrEgRDM8N0A5HJvxzp2OIaeSEL/Q==\"}";
