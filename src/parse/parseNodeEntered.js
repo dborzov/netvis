8d2492fa650ff01fdd550f21640ed29148aa1966
Peter Borzov <peter.borzov@gmail.com>
2014-12-24 16:31:31 +0500

    Flag nodes with nodeEntered events as not permanent

--- src/parse/parseNodeEntered.js
+++ src/parse/parseNodeEntered.js
@@ -1,7 +1,10 @@
 // parse NodeEntered event
 
 NetVis.prototype._parseNodeEntered = function(src) {
-  var r = this.Nodes.load({"id": src.name});
+  var r = this.Nodes.load({
+    "id": src.name,
+    "permanentNode": false
+  });
   var e = this.history.loadEvent(src, moment(src.time));
   return r;
 };
