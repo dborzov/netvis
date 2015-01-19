faa5ffd5b7a929515ec7a493ac43a364111d2f7e
Peter Borzov <peter.borzov@gmail.com>
2015-01-19 16:49:55 +0500

    Show messages as dots on bezier lines of connections

--- src/view/message.js
+++ src/view/message.js
@@ -1,2 +1,23 @@
 // view/message.js
-// Defines render() function for messages 
+// Defines render() function for messages
+
+
+NetVis.prototype.drawMessageCX = function(d) {
+  alphaX = d.source._xAbs;
+  betaX = d.destination._xAbs;
+  aX = 300;
+  t = d._p;
+  GY = alphaX + 2*t*(aX - alphaX) + (alphaX + betaX - 2 *aX)*t*t;
+  console.log("MessageCX reporting in: ", GY);
+  return GY;
+};
+
+NetVis.prototype.drawMessageCY = function(d) {
+  alphaX = d.source._yAbs;
+  betaX = d.destination._yAbs;
+  aX = 300;
+  t = d._p;
+  Gy =alphaX + 2*t*(aX - alphaX) + (alphaX + betaX - 2*aX)*t*t;
+  console.log("MessageCY reporting in: ", Gy);
+  return alphaX + 2*t*(aX - alphaX) + (alphaX + betaX - 2*aX)*t*t;
+};
