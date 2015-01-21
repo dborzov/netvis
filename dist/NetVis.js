984446003d0639eef0bf3e966bd79a427b197141
Peter Borzov <peter.borzov@gmail.com>
2015-01-21 14:31:44 +0500

    Sync up built files

--- dist/NetVis.js
+++ dist/NetVis.js
@@ -530,8 +530,10 @@ NetVis.prototype.render = function() {
     .attr('class','connection');
 
 
-  messages = canvas.selectAll('line.message').data(self._selectedTimeInterval.messages)
-    .enter().append('line')
+  messages = canvas.selectAll('path.message').data(self._selectedTimeInterval.messages)
+    .enter().append('path')
+    .attr("fill","transparent")
+    .attr("stroke","black")
     .on("click",function(d) { self._selected = d; self.render();})
     .attr('class','message');
 
@@ -554,20 +556,12 @@ NetVis.prototype.render = function() {
 
   syncPositions = function() {
     connections
-      .attr("d", function(d) {
-        from = "M" + d.connectingNode._xAbs + " " + d.connectingNode._yAbs + " ";
-        curve = "C " + 0.5*width + " " + 0.5*width + " " +  0.5*width + " "+ 0.5*width;
-        to = " " + d.dialedNode._xAbs + " " + d.dialedNode._yAbs;
-        return from + curve + to;
-      });
+      .attr("d", self.drawConnection);
 
 
     messages
-    .attr("x1", function(d) {return d.source._xAbs;})
-    .attr("y1", function(d) {return d.source._yAbs;})
-    .attr("x2", function(d) {return d.destination._xAbs;})
-    .attr("y2", function(d) {return d.destination._yAbs;});
-
+      .attr("d", self.drawMessage);
+    
 
     messagesAnimation
       .attr("cx", self.drawMessageCX)
@@ -769,6 +763,20 @@ NetVis.prototype.initView = function() {
 				c = self._width*0.5;
 				t = d._p;
 				return Math.pow(1-t,3)*p0 + 3*(1-t)*t*c + Math.pow(t,3)*p3;
+			};
+
+			self.drawConnection = function(d) {
+				from = "M" + d.connectingNode._xAbs + " " + d.connectingNode._yAbs + " ";
+				curve = "C " + 0.5*self._width + " " + 0.5*self._width + " " +  0.5*self._width + " "+ 0.5*self._width;
+				to = " " + d.dialedNode._xAbs + " " + d.dialedNode._yAbs;
+				return from + curve + to;
+			};
+
+			self.drawMessage = function(d) {
+				from = "M" + d.source._xAbs + " " + d.source._yAbs + " ";
+				curve = "C " + 0.5*self._width + " " + 0.5*self._width + " " +  0.5*self._width + " "+ 0.5*self._width;
+				to = " " + d.destination._xAbs + " " + d.destination._yAbs;
+				return from + curve + to;
 			};
 
 			// draw canvas
